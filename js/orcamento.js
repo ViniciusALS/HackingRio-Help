db0cf848f2012f551f2a0ceaee1d2b72d78f242f
Vinicius Souza <me@viniciusals.com>
2020-05-24 06:25:26 -0300

    Cria dinamismo na pagina do lojista

--- js/orcamento.js
+++ js/orcamento.js
@@ -1,4 +1,12 @@
 function orcamento(event) {
-    event.preventDefault()
+    event.preventDefault();
     alert("Orçamento da entrega: R$20,00");
+
+    document.getElementById("panel1").style.display = "none";
+    document.getElementById("panel2").style.display = "block";
+
+    setTimeout(() => {
+        document.getElementById("panel2").style.display = "none";
+        document.getElementById("panel3").style.display = "block";
+    }, 3000);
 }
